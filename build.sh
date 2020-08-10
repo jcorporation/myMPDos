88505a21d2e8b6a1a2476a0dad2e1a9b51875449
jcorporation <mail@juergenmang.de>
2020-08-11 00:43:48 +0200

    Fix: overall build improvements

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ ALPINE_MAJOR_VERSION="3.12"
 ALPINE_VERSION="${ALPINE_MAJOR_VERSION}.0"
 ALPINE_MIRROR="http://dl-cdn.alpinelinux.org/alpine"
 BUILDUSER=$USER
+IMAGESIZE=256
+IMAGESIZEBUILD=2048
+CLEANUP=0
 
 install -d tmp
 cd tmp || exit 1
@@ -14,18 +17,18 @@ cd tmp || exit 1
 echo "Download"
 if [ ! -f alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz ]
 then
-  wget "${ALPINE_MIRROR}/v${ALPINE_MAJOR_VERSION}/releases/${ARCH}/alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz" \
-	-O alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz
-  tar -xzf /alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz
+  wget -q "${ALPINE_MIRROR}/v${ALPINE_MAJOR_VERSION}/releases/${ARCH}/alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz" \
+	-O "alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz"
+  tar -xzf "alpine-netboot-${ALPINE_VERSION}-${ARCH}.tar.gz"
 fi
 
 [ -f alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz ] || \
-	wget "${ALPINE_MIRROR}/v${ALPINE_MAJOR_VERSION}/releases/${ARCH}/alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz" \
-		-O alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz
+	wget -q "${ALPINE_MIRROR}/v${ALPINE_MAJOR_VERSION}/releases/${ARCH}/alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz" \
+		-O "alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz"
 
 echo "Create build image"
-dd if=/dev/zero of="$IMAGE" bs=1M count=2048
-fdisk "$IMAGE" << EOL
+dd if=/dev/zero of="$IMAGE" bs=1M count="$IMAGESIZEBUILD"
+fdisk "$IMAGE" > /dev/null << EOL
 n
 p
 1
@@ -42,17 +45,18 @@ p
 w
 
 EOL
-LOOP=$(sudo losetup --partscan --show -f $IMAGE)
+LOOP=$(sudo losetup --partscan --show -f "$IMAGE")
 [ "$LOOP" = "" ] && exit 1
 sudo mkfs.vfat "${LOOP}p1"
 sudo mkfs.ext4 "${LOOP}p2"
 install -d mnt
-sudo mount -ouid=$BUILDUSER "${LOOP}p1" mnt || exit 1
+sudo mount -ouid="$BUILDUSER" "${LOOP}p1" mnt || exit 1
 tar -xzf alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz -C mnt
 cp boot/modloop-lts mnt/boot
 install -d mnt/mympd
 cp -r ../mympd/build/* mnt/mympd
-[ -d ../mympd-os-apks/.abuild ] && cp -r ../mympd-os-apks/.abuild mnt/mympd/
+[ -f ../mympd-os-apks/abuild.tgz ] && cp ../mympd-os-apks/abuild.tgz mnt/mympd/
+date +%s > mnt/date
 sudo umount mnt || exit 1
 
 rm -f init
@@ -66,21 +70,21 @@ qemu-system-aarch64 \
   -kernel boot/vmlinuz-lts -initrd boot/initramfs-lts \
   -append "console=ttyAMA0 ip=dhcp" \
   -nographic \
-  -drive file=${IMAGE},format=raw \
+  -drive "file=${IMAGE},format=raw" \
   -netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9 \
   -nic user,id=mynet0
 
 echo "Saving packages"
 install -d ../mympd-os-apks
 sudo mount -text4 "${LOOP}p2" mnt || exit 1
+[ -f mnt/build/abuild.tgz ] && cp mnt/build/abuild.tgz ../mympd-os-apks/
 cp mnt/build/packages/package/${ARCH}/* ../mympd-os-apks/
-cp -r mnt/build/.abuild ../mympd-os-apks/
 sudo umount mnt || exit 1
 sudo losetup -d "${LOOP}"
 
 echo "Create image"
-dd if=/dev/zero of="$IMAGE" bs=1M count=256
-fdisk "$IMAGE" << EOL
+dd if=/dev/zero of="$IMAGE" bs=1M count="$IMAGESIZE"
+fdisk "$IMAGE" > /dev/null << EOL
 n
 p
 1
@@ -92,21 +96,23 @@ a
 w
 
 EOL
-LOOP=$(sudo losetup --partscan --show -f $IMAGE)
+LOOP=$(sudo losetup --partscan --show -f "$IMAGE")
 [ "$LOOP" = "" ] && exit 1
 sudo mkfs.vfat "${LOOP}p1"
-sudo mount -ouid=$BUILDUSER "${LOOP}p1" mnt || exit 1
-tar -xzf alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz -C mnt
+sudo mount -ouid="$BUILDUSER" "${LOOP}p1" mnt || exit 1
+tar -xzf "alpine-rpi-${ALPINE_VERSION}-${ARCH}.tar.gz" -C mnt
 cd ../mympd/overlay || exit 1
-tar -czf ../../tmp/mnt/headless.apkovl.tar.gz *
+tar -czf ../../tmp/mnt/headless.apkovl.tar.gz .
 cd ../../tmp || exit 1
 echo "ssid WPA-PSK password" > mnt/wifi.txt
 cp -r ../mympd-os-apks mnt/
 sudo umount mnt || exit 1
 sudo losetup -d "${LOOP}"
 
+[ "$CLEANUP" = "0" ] && exit 0
 echo "Cleanup"
-#mv "$IMAGE" ..
-#cd ..
-#gzip "$IMAGE"
-#rm -r tmp
+mv "$IMAGE" ..
+cd ..
+gzip "$IMAGE"
+rm -r tmp
+exit 0
--- mympd/build/build.sh
+++ mympd/build/build.sh
@@ -1,62 +1,97 @@
 #!/bin/sh
 BUILDDIR="/usr/build"
-BRANCH="devel"
+MYMPD_BRANCH="devel"
+POWEROFF="0"
 
-while ! ntpd -q -n -p pool.ntp.org
-do
-  echo "Retry"
-done
+#Build packages
+B_MYMPD="0"
+B_MPD_STABLE="0"
+B_MPD_MASTER="1"
 
-setup-alpine -e -f /media/vda1/mympd/setup-alpine.answers
+echo ""
+echo "Starting myMPDos build"
 
+echo "Setting the local clock"
+date -s@$(cat /media/vda1/date)
+
+echo "Setting keymap"
+setup-keymap de de-nodeadkeys
+
+echo "Setup repositories and upgrade"
+setup-apkrepos -1
 sed -r -e's/^#(.*\d\/community)/\1/' -i /etc/apk/repositories
-apk update
 apk upgrade
-apk add e2fsprogs
 
-install -d /media/vda2
-mount /dev/vda2 /media/vda2 -text4
-cp -a /usr/* /media/vda2
-umount /media/vda2
+echo "Moving /usr to /dev/vda2"
+mount /dev/vda2 /mnt -text4
+cp -a /usr/* /mnt
+umount /mnt
 mount /dev/vda2 /usr -text4
 
-apk add git sudo alpine-sdk perl build-base
+echo "Setup apkcache"
+install -d /usr/build/distfiles -g abuild -m775
+setup-apkcache /usr/build/distfiles
 
+echo "Installing build packages"
+apk add git alpine-sdk perl sudo build-base
+
+echo "Adding build user"
 adduser -D build -h "$BUILDDIR"
 addgroup build abuild
 echo "build    ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-if [ -d /media/vda1/mympd/.abuild ]
-then
- cp -r /media/vda1/mympd/.abuild /usr/build/
- chown -R build.abuild /usr/build/.abuild
- chmod 700 /usr/build/.abuild
- chmod 600 /usr/build/.abuild/*.rsa
- chmod 644 /usr/build/.abuild/*.rsa.pub
- chmod 644 /usr/build/.abuild/abuild.conf
- cp /usr/build/.abuild/*.rsa.pub /etc/apk/keys/
-else
-  su build -c "abuild-keygen -n -a -i"
-fi
-
-install -d /var/cache/distfiles -g abuild -m775
-
 cd "$BUILDDIR" || exit 1
 
-su build -c "git clone -b $BRANCH --depth=1 https://github.com/jcorporation/myMPD.git"
-cd myMPD || exit 1
-#su build -c "./build.sh pkgalpine"
-cd ..
+echo "Setting up package signing key"
+if [ -f /media/vda1/mympd/abuild.tgz ]
+then
+  echo "Restoring .abuild"
+  tar -xzf /media/vda1/mympd/abuild.tgz
+  
+else
+  su build -c "abuild-keygen -n -a"
+  tar -czf abuild.tgz .abuild
+fi
+cp .abuild/*.rsa.pub /etc/apk/keys/
 
-#su build -c "cp -r /media/vda1/mympd/mpd-master ."
-#cd mpd-master || exit 1
-#su build -c "abuild checksum"
-#su build -c "abuild -r"
-#cd ..
+if [ "$B_MYMPD" = "1" ]
+then
+  echo "Building myMPD"
+  su build -c "git clone -b $MYMPD_BRANCH --depth=1 https://github.com/jcorporation/myMPD.git"
+  cd myMPD || exit 1
+  su build -c "./build.sh pkgalpine"
+  cd ..
+fi
+if [ "$B_MPD_MASTER" = "1" ]
+then
+  echo "Build MDP master"
+  MPDVER="0.22.0"
+  su build -c "cp -r /media/vda1/mympd/mpd-master ."
+  cd mpd-master || exit 1
+  sed -e "s/__MPDVER__/${MPDVER}/g" -i APKBUILD
+  su build -c "git clone -b master --depth=1 https://github.com/MusicPlayerDaemon/MPD.git"
+  mv MPD "mympd-os-mpd-master-${MPDVER}"
+  tar -czf mympd-os-mpd-master.tar.gz "mympd-os-mpd-master-${MPDVER}"
+  rm -fr "mympd-os-mpd-master-${MPDVER}"
+  su build -c "abuild checksum"
+  su build -c "abuild -r"
+  cd ..
+fi
+if [ "$B_MPD_STABLE" = "1" ]
+then
+  echo "Building MPD stable"
+  MPDVER="0.21.25"
+  su build -c "cp -r /media/vda1/mympd/mpd-stable ."
+  cd mpd-stable || exit 1
+  sed -e "s/__MPDVER__/${MPDVER}/g" -i APKBUILD
+  su build -c "wget http://www.musicpd.org/download/mpd/0.21/mpd-${MPDVER}.tar.xz"
+  tar -xzf "mpd-${MPDVER}.tar.xz"
+  rm "mpd-${MPDVER}.tar.xz"
+  mv "mpd-${MPDVER}" "mympd-os-mpd-stable-${MPDVER}"
+  tar -czf mympd-os-mpd-stable.tar.gz "mympd-os-mpd-stable-${MPDVER}"
+  rm -fr "mympd-os-mpd-stable-${MPDVER}"
+  su build -c "abuild checksum"
+  su build -c "abuild -r"
+  cd ..
+fi
 
-#su build -c "cp -r /media/vda1/mympd/mpd-stable ."
-#cd mpd-stable || exit 1
-#su build -c "abuild checksum"
-#su build -c "abuild -r"
-#cd ..
-
-#poweroff
+[ "$POWEROFF" = "1" ] && poweroff
