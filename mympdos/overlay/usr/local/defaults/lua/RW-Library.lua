9d1d97834c0aca9ee36178b78a8b2faf346debf8
jcorporation <mail@juergenmang.de>
2020-08-16 23:21:04 +0200

    Feat: improve lua scripts

--- mympdos/overlay/usr/local/defaults/lua/RW-Library.lua
+++ mympdos/overlay/usr/local/defaults/lua/RW-Library.lua
@@ -1,3 +1,3 @@
 -- {"order":1,"arguments":[]}
-local result = os.capture("/media/mmcblk0p2/scripts/rwlibrary.sh")
-return(result)
+result = mympd.os_capture("/media/mmcblk0p2/scripts/rwlibrary.sh")
+return result
