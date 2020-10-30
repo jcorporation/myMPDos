5bc608fdd87a5f7e7ca7b85e46716af1187b24ea
jcorporation <mail@jcgames.de>
2020-10-30 10:14:32 +0100

    Upd: update myMPDos from 0.1.0 to 0.2.0

--- updates/0.1.0/update/update.sh
+++ updates/0.1.0/update/update.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "Updating alpine base image to 3.12.1"
+echo " - Downloading"
 ARCHIVE="alpine-rpi-3.12.1-aarch64.tar.gz"
 CHECKSUM="125fcbc1e21d092dd68df6e457fa5e0b76964d00ffd7706e93c9eb9578f4f729"
 if wget -q "http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/aarch64/$ARCHIVE" -O "$ARCHIVE"
 then
-  if echo "$CHECKSUM $ARCHIVE" | sha256sum -c
+  echo " - Checking"
+  if echo "$CHECKSUM  $ARCHIVE" | sha256sum -c -w
   then
+    echo " - Extracting"
     mount -oremount,rw /media/mmcblk0p1
     tar -xzf "$ARCHIVE" -C /media/mmcblk0p1
     mount -oremount,ro /media/mmcblk0p1
+    echo " - Cleaning up"
     rm "$ARCHIVE"
-    rm "${ARCHIVE}.sha256"
   else
     echo "Checksum error"
     exit 1
