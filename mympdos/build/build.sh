c02629c4dcb21f6defa3943148b693d03e1ce298
jcorporation <mail@jcgames.de>
2020-11-16 23:20:48 +0100

    Upd: myMPD 6.8.0

--- mympdos/build/build.sh
+++ mympdos/build/build.sh
@@ -12,7 +12,8 @@ ARCH=$(uname -m)
 #Build packages
 B_BUILD="1"
 B_MYMPD="1"
-B_MYMPD_BRANCH="master"
+#B_MYMPD_BRANCH="master"
+B_MYMPD_BRANCH="devel"
 B_LIBMPDCLIENT="1"
 B_MPD_STABLE="1"
 B_MPD_MASTER="1"
