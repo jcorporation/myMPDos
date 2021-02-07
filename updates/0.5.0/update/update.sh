82dca232e247720d7bf93d874f1e9b9076e4bff5
jcorporation <mail@jcgames.de>
2021-02-07 20:46:24 +0100

    Upd: add update to 0.6.0 -> Alpine Linux 3.13.1

---
+++ updates/0.5.0/update/update.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+set -u
+
+echo "Updating alpine boot image to 3.13.1"
+echo " - Downloading"
+BOOTDEV="/media/mmcblk0p1"
+ARCHIVE="alpine-rpi-3.13.1-aarch64.tar.gz"
+CHECKSUM="bccaaab7cb4a167ed05e18d72fdde71b101568c03d94fee60c193c019f4b0944"
+if wget -q "http://dl-cdn.alpinelinux.org/alpine/v3.13/releases/aarch64/$ARCHIVE" -O "$ARCHIVE"
+then
+  echo " - Checking"
+  if echo "$CHECKSUM  $ARCHIVE" | sha256sum -c -w
+  then
+    echo " - Mounting $BOOTDEV rw"
+    mount -oremount,rw "$BOOTDEV"
+    echo " - Removing old boot files"
+    rm -rf "${BOOTDEV}/apks"
+    echo " - Extracting"
+    tar -xzf "$ARCHIVE" -C "$BOOTDEV"
+    echo " - Mounting $BOOTDEV ro"
+    mount -oremount,ro "$BOOTDEV"
+    echo " - Cleaning up"
+    rm "$ARCHIVE"
+  else
+    echo "Checksum error"
+    exit 1
+  fi
+else
+  echo "Error downloading alpine base image"
+  exit 1
+fi
