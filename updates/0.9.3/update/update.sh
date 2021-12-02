ebb52ade3a6c4ce2f46a6b2de317fcbb5fa0ef65
jcorporation <mail@jcgames.de>
2021-12-02 21:15:42 +0100

    Upd: replace sudo with doas

--- updates/0.9.3/update/update.sh
+++ updates/0.9.3/update/update.sh
@@ -3,4 +3,27 @@ export V_MAJOR=3
 export V_MINOR=15
 export V_POINT=0
 export CHECKSUM=5b1b42973294325fca4581eeda62d30d7a4741c7946234956b32ab98cda7b4fe
+
+#remove old lua scripts with sudo
+rm -f /var/lib/mympd/scripts/Shutdown.lua
+rm -f /var/lib/mympd/scripts/Reboot.lua
+rm -f /var/lib/mympd/scripts/RW-Library.lua
+
+#upgrade
 alpine-upgrade.sh
+
+#replace sudo with doas
+apk del sudo
+apk add doas
+
+#add doas config
+cat > /etc/doas.d/mympd.conf << EOL
+permit nopass mympd cmd /usr/bin/savereboot.sh
+permit nopass mympd cmd /usr/bin/saveshutdown.sh
+permit nopass mympd cmd /usr/bin/rwdata.sh
+
+EOL
+
+#remove sudo config
+rm -f /etc/sudoers
+rm -f /etc/sudoers.apk-new
