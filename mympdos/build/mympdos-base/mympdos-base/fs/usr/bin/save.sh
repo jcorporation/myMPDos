254516b5de3bbd1fae36e43e64384c3b731dddec
jcorporation <mail@jcgames.de>
2024-04-20 15:11:33 +0200

    Upd: save swclock Fix: wait for myMPD to save the state files #39

--- mympdos/build/mympdos-base/mympdos-base/fs/usr/bin/save.sh
+++ mympdos/build/mympdos-base/mympdos-base/fs/usr/bin/save.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
 # SPDX-License-Identifier: GPL-3.0-or-later
-# myMPDos (c) 2020-2023 Juergen Mang <mail@jcgames.de>
+# myMPDos (c) 2020-2024 Juergen Mang <mail@jcgames.de>
 # https://github.com/jcorporation/myMPDos
+
+MTIME_OLD=$(stat -c%Y /var/lib/mympd/state/trigger_list)
+MTIME_NEW=$MTIME_OLD
 
 # Write myMPD state to disc
 killall -HUP mympd
-#myMPD needs some time to write the state files
-sleep 1
 
 # Save the alsa state
 alsactl store
+
+# Update swclock time
+touch /var/lib/misc/openrc-shutdowntime
+
+# Give myMPD some time to write the state files
+I=0
+while [ "$MTIME_OLD" -eq "$MTIME_NEW" ]
+do
+    I=$((I+1))
+    sleep "$I"
+    MTIME_NEW=$(stat -c%Y /var/lib/mympd/state/trigger_list)
+    [ "$I" -eq 10 ] && break
+done
 
 # Save the state to /media/mmcblk0p2/
 lbu_commit
