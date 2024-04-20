218e2e1f4ffb18fbacfbceace03bbb50d1464c49
jcorporation <mail@jcgames.de>
2024-04-20 17:30:43 +0200

    Upd; mympdos-base 0.9.5

--- mympdos/build/mympdos-base/mympdos-base/fs/usr/bin/save.sh
+++ mympdos/build/mympdos-base/mympdos-base/fs/usr/bin/save.sh
@@ -8,23 +8,28 @@ MTIME_OLD=$(stat -c%Y /var/lib/mympd/state/trigger_list)
 MTIME_NEW=$MTIME_OLD
 
 # Write myMPD state to disc
+echo "Saving myMPD state"
 killall -HUP mympd
 
+echo "Saving alsa state"
 # Save the alsa state
 alsactl store
 
 # Update swclock time
+echo "Saving swclock"
 touch /var/lib/misc/openrc-shutdowntime
 
 # Give myMPD some time to write the state files
+echo "Waiting for myMPD"
 I=0
 while [ "$MTIME_OLD" -eq "$MTIME_NEW" ]
 do
     I=$((I+1))
-    sleep "$I"
+    sleep 1
     MTIME_NEW=$(stat -c%Y /var/lib/mympd/state/trigger_list)
     [ "$I" -eq 10 ] && break
 done
 
 # Save the state to /media/mmcblk0p2/
+echo "Commiting changes"
 lbu_commit
