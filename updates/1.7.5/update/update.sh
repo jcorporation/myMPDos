298eb0807263cf90dff931c7da5a9898fd34e5d7
jcorporation <mail@jcgames.de>
2025-12-29 17:17:01 +0100

    Fix: Update to 1.8.0

--- updates/1.7.5/update/update.sh
+++ updates/1.7.5/update/update.sh
@@ -2,10 +2,10 @@
 
 export V_MAJOR=3
 export V_MINOR=23
-export V_POINT=3
+export V_POINT=2
 export CHECKSUM=ff36ced256e6aafe8d921dd2227f69980b6d39937b238f65696e27f53ee9301f
 alpine-upgrade.sh
 
 # Replace custom libgpiod2 with official Alpine Linux package
-apk remove mympdos-libgpiod2
+apk del mympdos-libgpiod2
 apk add libgpiod mygpiod
